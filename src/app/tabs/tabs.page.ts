39993128bc7b64d7e4d36753088e215a635fe335
ajeet rajbhar <ajeet.offee@gmail.com>
2023-02-19 21:02:45 +0530

    init2

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tabs',
@@ -6,11 +8,52 @@ import { Component } from '@angular/core';
   styleUrls: ['tabs.page.scss']
 })
 export class TabsPage {
-  stepTogo = false
-  LetsGo()
-  {
-    this.stepTogo = true
+  result:string = ""
+  constructor(private actionSheetCtrl: ActionSheetController,public router:Router) {}
+
+  async presentActionSheet() {
+    const actionSheet = await this.actionSheetCtrl.create({
+      header: 'Create +',
+      // subHeader: 'Example subheader',
+      buttons: [
+        {
+          text: 'Upload a video',
+          role: 'upload',
+          data: {
+            action: 'upload',  
+          },
+        },
+        {
+          text: 'Go live',
+          role: 'live',
+          data: {
+            action: 'live',
+          },
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          data: {
+            action: 'cancel',
+          },
+        },
+      ],
+    });
+
+    await actionSheet.present();
+
+    const result = await actionSheet.onDidDismiss();
+    this.result = JSON.stringify(result, null, 2);
+    console.log(this.result);
+    if (result.role == 'upload') {
+      this.router.navigate(['/uploadVideo'])
+    }
+    else if (result.role == 'live') {
+      this.router.navigate(['/tab2'])
+    }
+       
+
+    
   }
-  constructor() {}
 
 }
