906f5507a80275028decf1e16e38aa4eb5656d19
ajeet rajbhar <ajeet.offee@gmail.com>
2023-02-19 16:47:46 +0530

    init1

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -3,6 +3,7 @@ import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 import { IonRouterOutlet, Platform } from '@ionic/angular';
 import { App } from '@capacitor/app';
+import { ItemReorderEventDetail } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -165,5 +166,16 @@ export class Tab1Page {
     return this.sanitizer.bypassSecurityTrustResourceUrl(url);
   }
 
+
+  handleReorder(ev: CustomEvent<ItemReorderEventDetail>) {
+    // The `from` and `to` properties contain the index of the item
+    // when the drag started and ended, respectively
+    console.log('Dragged from index', ev.detail.from, 'to', ev.detail.to);
+
+    // Finish the reorder and position the item in the DOM based on
+    // where the gesture ended. This method can also be called directly
+    // by the reorder group
+    ev.detail.complete();
+  }
 
 }
