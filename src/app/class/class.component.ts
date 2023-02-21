e7def06177be28a747807adecbd4d5f2cc5a1cfe
ajeet rajbhar <ajeet.offee@gmail.com>
2023-02-21 22:55:01 +0530

    init

--- src/app/class/class.component.ts
+++ src/app/class/class.component.ts
@@ -6,9 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./class.component.scss'],
 })
 export class ClassComponent implements OnInit {
-
+  classes:any= [
+    { classNamme : 'Standard 1', classId : 1, std_icon : "assets/std_icon.webp"},
+    { classNamme : 'Standard 2', classId : 2, std_icon : "assets/std_icon.webp"},
+    { classNamme : 'Standard 3', classId : 3, std_icon : "assets/std_icon.webp"},
+    { classNamme : 'Standard 4', classId : 4, std_icon : "assets/std_icon.webp"},
+    { classNamme : 'Standard 5', classId : 5, std_icon : "assets/std_icon.webp"}
+  ]
   constructor() { }
-
+  
+  setOpen(isOpen: boolean, video: any) {
+  }
+   
   ngOnInit() {}
 
 }
